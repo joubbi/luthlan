a680e7e504e9a8c48f4e98757d3e5a287d057f7f
Farid Joubbi <farid@joubbi.se>
2019-02-18 13:44:12 +0100

    added DHCP and more output

--- luthlan.sh
+++ luthlan.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
+if [ $# -lt 3 ]; then
+  echo "Wrong amount of arguments!"
+  echo "./`basename $0` device number address"
+  echo "Example: ./`basename $0` eth0 1 192.168.0.100"
+  echo
+  exit 1
+fi
+
 echo 1 >/proc/sys/net/ipv4/conf/all/arp_ignore
 echo 2 >/proc/sys/net/ipv4/conf/all/arp_announce
 echo 2 >/proc/sys/net/ipv4/conf/all/rp_filter
 
-#device="$1.$2"
 device="$1.$(printf %02d $2)"
 
-#ip link add link "$1" address 58:9c:fc:00:11:"$2" $device type macvlan
 ip link add link "$1" address 58:9c:fc:00:11:"$(printf %02d $2)" $device type macvlan
-ip addr add 192.168.0.11/24 brd + dev $device
 ip link set $device up
 
+if [ $3 = "dhcp" ]; then
+  dhclient $device
+else
+  ip addr add $3 brd + dev $device
+fi
+
 echo "$device created"
+ip addr show dev $device
+
