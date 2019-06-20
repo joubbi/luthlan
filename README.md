f27ce7c2d6df44fb35fb3b7236ca37c0d83212cc
Farid Joubbi <farid@joubbi.se>
2019-06-20 13:15:39 +0200

    Added VLAN support and made it interactive.

--- README.md
+++ README.md
@@ -3,20 +3,33 @@
 A script for adding macvlan interfaces on a linux device.
 
 Each device will communicate using it's own hardware address.
-The hardware address is `58:9c:fc:00:11:x`, where x is the same as the device number supploed to the script.
+You can either pass in values as arguments to the scripts, or run it interactively. 
+
+### VLAN support
+The script has support for VLANs.
+You can for example configure a Cisco switchport as a VLAN trunk:
+```
+interface GigabitEthernet1/0/5
+ description luthlan
+ switchport trunk allowed vlan 600,1100-1999
+ switchport mode trunk
+``` 
 
 
 ### Usage
 
-To create a device named eth0.1 with the IP address 192.168.0.100/24:
+First. Make sure that the variable hw_device is set to the right interface.
+You need to edit the script to set this. It's at the top of the script.
 
-`$ ./luthlan.sh eth0 1 192.168.0.100/24`
+To create 200 devices named eno1.100.x with the IP address 192.168.1.2/24 in VLAN 100, where x is the device number 1-200:
+
+`$ ./luthlan.sh 1 200 192.168.1.2/24 100
 
 
 
-To create a device named eth0.2 with the IP address obtained from DHCP:
+To create a device named eno1.1 with the IP address obtained from DHCP:
 
-`$ ./luthlan.sh eth0 2 dhcp`
+`$ ./luthlan.sh 1 1 dhcp
 
 
 ___
--- luthlan.sh
+++ luthlan.sh
@@ -4,38 +4,85 @@
 # A script for adding macvlan interfaces on a linux device.                   #
 # Each device will communicate using it's own hardware address.               #
 #                                                                             #
+# You can either pass in values as arguments to the scripts,                  #
+# or run it interactively.                                                    #
+# The variables are the following:                                            #
+# "first device number" "amount of devices" "first address" "vlan"            #
+# Example: ./luthlan.sh 1 200 192.168.1.2/24 100                              #
+#                                                                             #
+# Version 2.0 2019-06-20 Added support for VLANs and read from user.          #
 # Version 1.0 2019-02-18 Initial release.                                     #
 #                                                                             #
 # Licensed under the Apache License Version 2.0                               #
 # Written by farid@joubbi.se                                                  #
 #                                                                             #
-############################################################################### 
+###############################################################################
 
+hw_device=eno1
 
-
-if [ $# -lt 3 ]; then
-  echo "Wrong amount of arguments!"
-  echo "./`basename $0` device number address"
-  echo "Example: ./`basename $0` eth0 1 192.168.0.100/24"
-  echo
-  exit 1
-fi
 
 echo 1 >/proc/sys/net/ipv4/conf/all/arp_ignore
 echo 2 >/proc/sys/net/ipv4/conf/all/arp_announce
 echo 2 >/proc/sys/net/ipv4/conf/all/rp_filter
 
-device="$1.$(printf %02d $2)"
+first="$1"
+amount="$2"
+first_address="$3"
+vlan="$4"
 
-ip link add link "$1" address 58:9c:fc:00:11:"$(printf %02d $2)" $device type macvlan
-ip link set $device up
-
-if [ $3 = "dhcp" ]; then
-  dhclient $device
-else
-  ip addr add $3 brd + dev $device
+if [ -z "$first" ]; then
+  read -rp 'First device number: ' first
 fi
 
-echo "$device created"
-ip addr show dev $device
+if [ -z "$amount" ]; then
+  read -rp 'Amount of devices: ' amount
+fi
+
+if [ -z "$first_address" ]; then
+  read -rp 'First IP address (with mask): ' first_address
+fi
+
+
+if [ -z "$vlan" ]; then
+  read -rp 'VLAN: ' vlan
+  if [ -z "$vlan" ]; then
+    vlan_device=$hw_device
+  fi
+fi
+if [ -n "$vlan" ]; then
+  modprobe 8021q
+  vconfig add "$hw_device" "$vlan"
+  vlan_device=$hw_device.$vlan
+  ip link set "$vlan_device" up
+fi
+echo "$vlan_device"
+
+
+three_first_octets=$(echo "$first_address" | grep -oE "\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}\b")
+last_octet=$(echo "$first_address" | grep -oP '[0-9]+(?=/)')
+mask=$(echo "$first_address" | grep -oP '(?<=/)[0-9]+')
+
+
+number="$first"
+while [ "$number" -lt $((first + amount)) ]
+do
+  device="$vlan_device.$number"
+  if ip link add link "$vlan_device" "$device" type macvlan ; then
+    echo "$device created"
+  else
+    echo "Something went wrong creating $device"
+  fi
+  ip link set "$device" up
+
+  if [ "$first_address" = "dhcp" ]; then
+    dhclient "$device"
+  else
+    address=$three_first_octets$last_octet"/"$mask
+    ip addr add "$address" dev "$device"
+  fi
+
+  #  ip addr show dev $device
+  number=$((number + 1))
+  last_octet=$((last_octet + 1))
+done
 
